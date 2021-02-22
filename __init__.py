fixed scaling of diagnostic plots--see IPSP and DISPLAY_DPI in __init__.py

__init__.py
@@ -9,6 +9,9 @@ import scipy.interpolate as spi
 import scipy.io as sio
 from octoblob import bmp_tools
 
+IPSP = 4.0
+DISPLAY_DPI = 50
+
 class ProcessingParameters:
 
     def __init__(self):
@@ -157,7 +160,7 @@ class OCTRawData:
             out[:,x] = np.roll(frame[:,x],-max_rise_index[x])
             
         if diagnostics:
-            plt.figure()
+            plt.figure(figsize=(1*IPSP,2*IPSP),dpi=DISPLAY_DPI)
             plt.subplot(2,1,1)
             plt.imshow(frame,cmap='gray',aspect='auto',interpolation='none')
             plt.axhspan(z1,z2,alpha=0.1)
@@ -202,13 +205,13 @@ class OCTRawData:
 
             if frame.max()>=self.saturation_value:
                 if diagnostics:
-                    plt.figure()
+                    plt.figure(figsize=(IPSP,IPSP),dpi=DISPLAY_DPI)
                     plt.hist(frame,bins=100)
                     plt.title('Frame saturated with pixels >= %d.'%self.saturation_value)
                 print('Frame saturated, with pixels >= %d.'%self.saturation_value)
             
             if diagnostics:
-                plt.figure()
+                plt.figure(figsize=(IPSP,2*IPSP),dpi=DISPLAY_DPI)
                 plt.subplot(2,1,1)
                 plt.hist(frame,bins=100)
                 plt.title('before %d bit shift'%self.bit_shift_right)
@@ -228,7 +231,7 @@ class OCTRawData:
 
 
             if diagnostics:
-                plt.figure()
+                plt.figure(figsize=(IPSP,IPSP),dpi=DISPLAY_DPI)
                 plt.imshow(frame,aspect='auto',interpolation='none')
                 plt.colorbar()
                 plt.title('raw data (bit shifted %d bits)'%self.bit_shift_right)
@@ -272,7 +275,7 @@ def dc_subtract(spectra,diagnostics=False):
     out = (spectra.T-dc).T
     
     if diagnostics:
-        plt.figure()
+        plt.figure(figsize=(2*IPSP,2*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(2,2,1)
         plt.imshow(spectra,aspect='auto',cmap='gray',interpolation='none')
         plt.colorbar()
@@ -324,7 +327,7 @@ def k_resample(spectra,coefficients=pp.k_resampling_coefficients,diagnostics=Fal
     interpolated[-1,:] = interpolated[-2,:]
 
     if diagnostics:
-        plt.figure()
+        plt.figure(figsize=(2*IPSP,3*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(3,2,1)
         plt.imshow(spectra)
         plt.colorbar()
@@ -367,7 +370,7 @@ def dispersion_compensate(spectra,coefficients=pp.dispersion_coefficients,diagno
     if diagnostics:
         before = 20*np.log10(np.abs(np.fft.fft(spectra,axis=0)))
         after = 20*np.log10(np.abs(np.fft.fft(dechirped,axis=0)))
-        plt.figure()
+        plt.figure(figsize=(2*IPSP,1*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(1,2,1)
         plt.imshow(before,cmap='gray',aspect='auto',clim=[40,80])
         plt.colorbar()
@@ -404,7 +407,7 @@ def gaussian_window(spectra,sigma=pp.gaussian_window_sigma,diagnostics=False):
             return dc,bright
 
             
-        plt.figure()
+        plt.figure(figsize=(2*IPSP,4*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(4,2,1)
         plt.imshow(np.abs(spectra),cmap='gray',aspect='auto',interpolation='none')
         plt.colorbar()
@@ -471,7 +474,7 @@ def spectra_to_bscan(spectra,oversampled_size=None,z1=None,z2=None,x1=None,x2=No
     #return np.fft.fft(spectra,axis=0,n=oversampled_size)[z1:z2]
     bscan = np.fft.fft(spectra,axis=0,n=oversampled_size)
     if diagnostics:
-        plt.figure()
+        plt.figure(figsize=(1*IPSP,1*IPSP),dpi=DISPLAY_DPI)
         plt.imshow(20*np.log10(np.abs(bscan)),cmap='gray',clim=[40,80],aspect='auto')
         plt.colorbar()
         plt.axhline(z1)
@@ -509,7 +512,7 @@ def show_bscan(bscan,title='',clim=None,plot_ascan=False):
     plt.colorbar()
     plt.title(title)
     if plot_ascan:
-        plt.figure()
+        plt.figure(figsize=(1*IPSP,1*IPSP),dpi=DISPLAY_DPI)
         plt.plot(display_bscan.mean(1))
     #plt.savefig('./figs/%s.png'%(title.replace(' ','_')))
 
@@ -685,7 +688,7 @@ def get_phase_jumps(phase_stack,mask,
 
 
     if diagnostics:
-        plt.figure(figsize=(8,6))
+        plt.figure(figsize=((n_reps-1)*IPSP,2*IPSP),dpi=DISPLAY_DPI)
         plt.suptitle('phase shifts between adjacent frames in cluster')
         for rep in range(1,n_reps):
             plt.subplot(2,n_reps-1,rep)
@@ -724,7 +727,7 @@ def get_phase_jumps(phase_stack,mask,
 
 
     if diagnostics:
-        plt.figure(figsize=(12,6))
+        plt.figure(figsize=((n_reps-1)*IPSP,1*IPSP),dpi=DISPLAY_DPI)
         total_bins = n_bins*resample_factor
         
         hist_sets = np.zeros((n_reps-1,n_fast,total_bins))
@@ -774,7 +777,7 @@ def bulk_motion_correct(phase_stack,mask,
     # Take a stack of B-scan phase arrays, with dimensions
     # (z,x,repeats), and return a bulk-motion corrected
     # version
-    
+
     n_reps = phase_stack.shape[2]
 
     b_jumps = get_phase_jumps(phase_stack,mask,
@@ -792,7 +795,7 @@ def bulk_motion_correct(phase_stack,mask,
         #err_clim = (np.min(np.sum(b_jumps,axis=1)),np.max(np.sum(b_jumps,axis=1)))
         phase_clim = (-np.pi,np.pi)
         err_clim = (-np.pi-np.min(-np.sum(b_jumps,axis=1)),np.pi+np.max(-np.sum(b_jumps,axis=1)))
-        plt.figure(figsize=(8,6))
+        plt.figure(figsize=((n_reps-1)*IPSP,2*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(2,n_reps+1,1)
         plt.imshow(mask*phase_stack[:,:,0],clim=phase_clim,aspect='auto',interpolation='none')
         plt.xticks([])
@@ -853,7 +856,7 @@ def phase_variance(data_phase,mask,diagnostics=False):
     # i.e. variance is computed with N-1 in denominator
     pv = np.var(np.exp(1j*data_phase),axis=2,ddof=1)
     if diagnostics:
-        plt.figure(figsize=(8,4))
+        plt.figure(figsize=(3*IPSP,1*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(1,3,1)
         plt.imshow(pv,cmap='gray',aspect='auto',interpolation='none')
         plt.xticks([])
@@ -911,7 +914,7 @@ def make_angiogram(stack_complex,bulk_correction_threshold=None,phase_variance_t
 
     if diagnostics:
         nbins=20
-        plt.figure(figsize=(12,8))
+        plt.figure(figsize=(4*IPSP,2*IPSP),dpi=DISPLAY_DPI)
         plt.subplot(2,4,1)
         plt.hist(np.ravel(stack_dB),bins=nbins)
         plt.xlabel('dB (full stack)')

registration_tools.py
@@ -49,7 +49,9 @@ def rigid_shift(ref,tar,max_shift=None,diagnostics=False,ref_pre_fft=False):
     # use rigid_register above and return a correctly shifted version of target (tar)
     
     dx,dy,xc = rigid_register(ref,tar,max_shift,diagnostics,ref_pre_fft)
-    new_tar = np.ones(tar.shape,dtype=tar.dtype)*np.min(tar)
+    #new_tar = np.ones(tar.shape,dtype=tar.dtype)*np.min(tar)
+    new_tar = np.zeros(tar.shape,dtype=tar.dtype)
+    
     print(dx,dy)
     
     if dx>0: