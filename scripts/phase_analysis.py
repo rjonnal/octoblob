63f5bc7d6327035aa02702cd6ce4b3f0e4a7ea4a
Ravi Sankar Jonnal <rjonnal@gmail.com>
2021-09-09 15:14:19 -0700

    fixed bugs in phase_analysis script

--- scripts/phase_analysis.py
+++ scripts/phase_analysis.py
@@ -10,6 +10,7 @@ import scipy
 print(scipy.__version__)
 
 from octoblob.registration_tools import rigid_register
+from octoblob.ticktock import tick,tock
 
 import logging
 logging.basicConfig(
@@ -21,11 +22,22 @@ logging.basicConfig(
     ]
 )
 
-block_size = 5
-histogram_threshold_fraction = 0.2
+histogram_threshold_fraction = 0.03
 signal_threshold_fraction = 0.05
+unfiltered_min = -5000
+unfiltered_max = 5000
+filtered_min = -2500
+filtered_max = 2500
+
+
+# the duration over which we assume the retina is stationary (in seconds)
+stationary_duration = 0.01
+testing = False
 
 phase_clim = (0,2*np.pi)
+
+def phase_to_nm(phase):
+    return phase/(4*np.pi*1.38)*1050.0
 
 # setup parameters
 unp_filename = sys.argv[1]
@@ -50,6 +62,8 @@ n_fast = n_fast*n_repeats
 # The same is true for n_depth.
 dt = n_fast/1e5 
 logging.info('B-scan interval: %0.1f ms; B-scan rate: %0.1f Hz'%(dt*1000,1/dt))
+block_size = int(round(stationary_duration/dt))
+logging.info('Setting block size to %d, assuming retina is stationary for %d ms.'%(block_size,stationary_duration*1000))
 
 folder_name = '%s_bscans'%unp_filename.replace('.unp','')
 filter_string = os.path.join(folder_name,'complex*.npy')
@@ -109,7 +123,6 @@ def strip_align_pair(a,b,n_strips,diagnostics=False):
         new_b.append(b_strip)
     return np.hstack(new_b)
 
-block_start = 0
 oversample_factor = (1,1)
 
 oversampled_dict = {}
@@ -122,56 +135,182 @@ def get_oversampled_file(fn,factor,odict):
         ov = oversample(ov,factor)
         odict[fn] = ov
     return ov
-        
+
+
+plt.figure(figsize=(10,4),dpi=100)
+
+
+### Figure out what the histogram max is, just for visualization:
+t0 = tick()
+temp_block_start = 0
+hist_max = 0
+while temp_block_start+block_size<=n_files:
+    files = flist[temp_block_start:temp_block_start+block_size]
+    ref = get_oversampled_file(files[0],oversample_factor,oversampled_dict)
+    
+    block = [ref]
+    for tar_idx in range(1,block_size):
+        tar = get_oversampled_file(files[tar_idx],oversample_factor,oversampled_dict)
+        block.append(tar)
+
+    block = np.array(block)
+    average_bscan = np.nanmean(np.abs(block),axis=0)
+    
+    signal_mask = np.zeros(average_bscan.shape)
+
+    # there may be nans, so use nanmax
+    signal_threshold = np.nanmax(average_bscan)*signal_threshold_fraction
+    
+    signal_mask[average_bscan>signal_threshold] = 1
+    hist_max_temp = np.sum(signal_mask)/8.0
+    if hist_max_temp>hist_max:
+        hist_max = hist_max_temp
+    temp_block_start = temp_block_start + 5
+    
+
+### Begin processing blocks:
+t0 = tick()
+block_start = 0
 while block_start+block_size<=n_files:
+    
     files = flist[block_start:block_start+block_size]
     ref = get_oversampled_file(files[0],oversample_factor,oversampled_dict)
     
-    logging.info('strip_align_pair: block %d'%block_start)
+    logging.info('processing block %d'%block_start)
 
     block = [ref]
     for tar_idx in range(1,block_size):
-        logging.info('strip_align_pair: images %d and %d'%(block_start,block_start+tar_idx))
         tar = get_oversampled_file(files[tar_idx],oversample_factor,oversampled_dict)
         registered = strip_align_pair(ref,tar,10)
         block.append(registered)
 
     block = np.array(block)
-    block = np.transpose(block,(1,2,0))
+    # in test set, depth is 200, fast is 100, and block size is 5
+    # block shape right now, after np.array, is 5, 200, 100
+    # let's keep this as our convention, following the volume averaging
+    # convention
 
-    average_bscan = np.mean(np.abs(block),axis=2)
+    if testing:
+        block[2,-80:-70,20:80] = np.nan
+        plt.imshow(np.abs(block[2,:,:]))
+        plt.title('Testing B-scan (with nan region).')
+        plt.show()
+        
+    average_bscan = np.nanmean(np.abs(block),axis=0)
+    
     histogram_mask = np.zeros(average_bscan.shape)
     signal_mask = np.zeros(average_bscan.shape)
-    histogram_threshold = np.max(average_bscan)*histogram_threshold_fraction
-    signal_threshold = np.max(average_bscan)*signal_threshold_fraction
+
+    # there may be nans, so use nanmax
+    histogram_threshold = np.nanmax(average_bscan)*histogram_threshold_fraction
+    signal_threshold = np.nanmax(average_bscan)*signal_threshold_fraction
     
     histogram_mask[average_bscan>histogram_threshold] = 1
     signal_mask[average_bscan>signal_threshold] = 1
     
     block_phase = np.angle(block)
 
-    corrected_block_phase = blob.bulk_motion_correct(block_phase,histogram_mask,diagnostics=False)
+    transposed = np.transpose(block_phase,(1,2,0))
+    corrected_block_phase = blob.bulk_motion_correct(transposed,histogram_mask,diagnostics=False)
+    corrected_block_phase = np.transpose(corrected_block_phase,(2,0,1))
 
-    corrected_block_phase = np.transpose(np.transpose(corrected_block_phase,(2,0,1))*signal_mask,(1,2,0))
-    
-    corrected_block_phase = np.unwrap(corrected_block_phase,axis=2)
-    corrected_block_phase = corrected_block_phase%(2*np.pi)
 
-    block_dphase = np.diff(corrected_block_phase,axis=2)
-    phase_slope = np.mean(block_dphase,axis=2)
-    phase_slope[np.where(signal_mask==0)] = np.nan
+    # the original version is below, and gets completely broken by nans:
+    #corrected_block_phase = corrected_block_phase%(2*np.pi)
+    #block_dphase = np.diff(corrected_block_phase,axis=2)
+    #phase_slope = np.mean(block_dphase,axis=2)
+    #phase_slope[np.where(signal_mask==0)] = np.nan
+
+    # nans mess this up:
+    #corrected_block_phase = np.unwrap(corrected_block_phase,axis=0)
+
+    # The following approach doesn't work because masking turns corrected_block_phase into a 1D array;
+    # the only way I can think of solving this problem is by for loop through the signal_mask pixels.
+    # Keep thinking of a way to vectorize.
+    #corrected_block_phase[~np.isnan(corrected_block_phase)] = np.unwrap(corrected_block_phase[~np.isnan(corrected_block_phase)],axis=0)
+
+    valid_depth,valid_fast = np.where(signal_mask==1)
     
+    t_vec = np.arange(block_size)*dt
+
+    phase_slope_image = np.ones(signal_mask.shape)*np.nan
+    nm_slope_image = np.ones(signal_mask.shape)*np.nan
+    nm_slope_image_filtered = np.ones(signal_mask.shape)*np.nan
+    high_vals = 0
+    high_vals2 = 0
+    for depth,fast in zip(valid_depth,valid_fast):
+        theta = corrected_block_phase[:,depth,fast]
+        theta_unwrapped = np.ones(theta.shape)*np.nan
+        nan_mask = ~np.isnan(theta)
+        theta_unwrapped[nan_mask] = np.unwrap(theta[nan_mask])
+        changed = any([a!=b for a,b in zip(theta[nan_mask],theta_unwrapped[nan_mask])])
+        if any(np.isnan(theta)) and changed:
+            logging.info('nan values found in phase ramp; repaired:')
+            logging.info('%s->%s'%(theta,theta_unwrapped))
+            
+        dt_vec = np.diff(t_vec[nan_mask])
+        dtheta_vec = np.diff(theta[nan_mask])
+
+        if True:
+            for k in range(len(dtheta_vec)):
+                v = dtheta_vec[k]
+                while np.abs(v-2*np.pi)<np.abs(v):
+                    v = v - 2*np.pi
+                while np.abs(v+2*np.pi)<np.abs(v):
+                    v = v + 2*np.pi
+                dtheta_vec[k] = v
+
+        nm_vec = phase_to_nm(dtheta_vec)
+        nm_slope = np.mean(nm_vec/dt_vec)
+        phase_slope = np.mean(dtheta_vec/dt_vec)
+        
+        phase_slope_image[depth,fast] = phase_slope
+        nm_slope_image[depth,fast] = nm_slope
+        
+        if filtered_min<nm_slope<filtered_max:
+            nm_slope_image_filtered[depth,fast] = nm_slope
+        
+        if np.abs(phase_slope)>1000:
+            high_vals+=1
+            
     plt.clf()
+    plt.subplot(1,3,1)
     plt.imshow(20*np.log10(average_bscan),cmap='gray',aspect='auto')
-    plt.imshow(phase_slope,cmap='jet',alpha=0.33,aspect='auto')
+    plt.imshow(nm_slope_image,cmap='jet',alpha=0.33,aspect='auto',clim=[-5000,5000])
     plt.colorbar()
-    plt.title(r'phase ramp (d$\theta$/dt), frames %02d-%02d (t = %0.3f, %0.3f)'%(block_start,block_start+block_size-1,dt*block_start,dt*(block_start+block_size-1)))
+    plt.title('phase ramp (dL/dt)(nm)')
+    plt.suptitle('frames %02d-%02d (t = %0.3f, %0.3f)'%(block_start,block_start+block_size-1,dt*block_start,dt*(block_start+block_size-1)))
+    plt.subplot(1,3,2)
+    plt.imshow(20*np.log10(average_bscan),cmap='gray',aspect='auto')
+    plt.imshow(nm_slope_image_filtered,cmap='jet',alpha=0.9,aspect='auto',clim=[unfiltered_min,unfiltered_max])
+    plt.colorbar()
+    plt.title('phase ramp (dL/dt)(nm) (filtered)')
+
+    
+    plt.subplot(1,3,3)
+    plt.hist(nm_slope_image[~np.isnan(nm_slope_image)],bins=np.arange(unfiltered_min,unfiltered_max,500))
+    plt.xlabel('dL/dt')
+    plt.ylabel('count')
+    plt.title('histogram')
+    plt.gca().yaxis.tick_right()
+    plt.gca().yaxis.set_label_position('right')
+    plt.ylim((0,hist_max))
+    plt.axvspan(filtered_min,filtered_max,alpha=0.25)
+    
     png_outfn = os.path.join(png_output_directory,'phase_ramp_frames_%02d-%02d.png'%(block_start,block_start+block_size-1))
     npy_outfn = os.path.join(npy_output_directory,'phase_ramp_frames_%02d-%02d.npy'%(block_start,block_start+block_size-1))
     plt.savefig(png_outfn,dpi=100)
-    np.save(npy_outfn,phase_slope)
+    np.save(npy_outfn,phase_slope_image)
     plt.pause(.0001)
 
 
     
     block_start+=1
+    ttemp = tock(t0)
+    bbs = block_start/ttemp
+    time_remaining = (n_files-block_start-block_size)/bbs
+    logging.info('processed %d blocks in %0.1f s (%0.2f blocks per second)'%(block_start,ttemp,bbs))
+    logging.info('time remaining: %d s'%time_remaining)
+    if testing:
+        logging.info('Testing set to True, so quitting after first iteration.')
+        sys.exit()
