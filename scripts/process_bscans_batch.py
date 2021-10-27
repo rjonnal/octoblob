53b012adf724ab6f45ba3b661dbf656871d96ca9
Ravi Sankar Jonnal <rjonnal@gmail.com>
2021-10-27 14:14:08 -0700

    batch versions for Windows--multiprocessing problem

--- scripts/process_bscans_batch.py
+++ scripts/process_bscans_batch.py
@@ -3,35 +3,36 @@ import parameters as params
 from process_bscans import process
 import octoblob as blob
 
-use_multiprocessing = params.use_multiprocessing
-if use_multiprocessing:
-    import multiprocessing as mp
+if __name__=='__main__':
+    use_multiprocessing = params.use_multiprocessing
+    if use_multiprocessing:
+        import multiprocessing as mp
 
-args = sys.argv[1:]
-args = blob.expand_wildcard_arguments(args)
+    args = sys.argv[1:]
+    args = blob.expand_wildcard_arguments(args)
 
-files = []
-flags = []
+    files = []
+    flags = []
 
-for arg in args:
-    if os.path.exists(arg):
-        files.append(arg)
-    else:
-        flags.append(arg.lower())
+    for arg in args:
+        if os.path.exists(arg):
+            files.append(arg)
+        else:
+            flags.append(arg.lower())
 
-diagnostics = 'diagnostics' in flags
-show_processed_data = 'show' in flags
+    diagnostics = 'diagnostics' in flags
+    show_processed_data = 'show' in flags
 
-files.sort()
+    files.sort()
 
-def proc(f):
-    process(f,diagnostics=diagnostics,show_processed_data=show_processed_data)
-
-if use_multiprocessing:
-    with mp.Pool(4) as p:
-        p.map(proc,files)
-        
-else:
-    for f in files:
+    def proc(f):
         process(f,diagnostics=diagnostics,show_processed_data=show_processed_data)
+
+    if use_multiprocessing:
+        with mp.Pool(4) as p:
+            p.map(proc,files)
+
+    else:
+        for f in files:
+            process(f,diagnostics=diagnostics,show_processed_data=show_processed_data)
 
