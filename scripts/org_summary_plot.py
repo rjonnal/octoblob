ac8d537a4857b928d2497301e0117c01d99b321b
Ravi Sankar Jonnal <rjonnal@gmail.com>
2022-01-05 11:18:05 -0800

    progress on summary plot for org

--- scripts/org_summary_plot.py
+++ scripts/org_summary_plot.py
@@ -8,34 +8,90 @@ df_all =  pd.read_csv('master_summary.csv')
 subject_array = [1,2,3]
 subject_marker_array = ['s','o','x']
 eccentricity_array = [2,4,6,8]
+bleaching_array = [8,17,33,66]
+eccentricity_marker_array = ['s','o','x','^']
 
 
-# first, let's look at eccentricity-dependence, with only 66% bleaching
-df = df_all[df_all['bleaching']==66]
+def err(vec):
+    return np.std(vec)/np.sqrt(len(vec))
 
-for idx,subject in enumerate(subject_array):
-    subject_df = df[df['subject']==subject]
-    subject_marker = subject_marker_array[idx]
-    color_marker = 'k'+subject_marker
+if True:
+    # first, let's look at eccentricity-dependence, with only 66% bleaching
+    df = df_all[df_all['bleaching']==66]
 
-    x_offset = (idx-1)*0.1
+    for idx,subject in enumerate(subject_array):
+        subject_df = df[df['subject']==subject]
+        subject_marker = subject_marker_array[idx]
+        color_marker = 'k'+subject_marker
 
-    for eccentricity in eccentricity_array:
+        x_offset = (idx-1)*0.1
 
-        if eccentricity==2:
-            label = 'subject %d'%subject
+        for eccentricity in eccentricity_array:
+
+            if eccentricity==2:
+                label = 'subject %d'%subject
+            else:
+                label = None
+
+            ecc_df = subject_df[subject_df['eccentricity']==eccentricity]
+
+            vmin_arr = np.array(ecc_df['velocity min'])
+            vmax_arr = np.array(ecc_df['velocity max'])
+            plt.figure(1)
+            plt.plot(eccentricity+x_offset,np.mean(vmin_arr),color_marker,label=label)
+            plt.errorbar(eccentricity+x_offset,np.mean(vmin_arr),yerr=err(vmin_arr),ecolor='k',capsize=4)
+            plt.xlabel('eccentricity (deg)')
+            plt.ylabel('$v_{min}$')
+            
+            plt.figure(2)
+            plt.plot(eccentricity+x_offset,np.mean(vmax_arr),color_marker,label=label)
+            plt.errorbar(eccentricity+x_offset,np.mean(vmax_arr),yerr=err(vmax_arr),ecolor='k',capsize=4)
+            plt.xlabel('eccentricity (deg)')
+            plt.ylabel('$v_{max}$')
+
+plt.figure(1)
+plt.legend()
+plt.figure(2)
+plt.legend()
+
+# done with df, so delete and reuse
+
+df = df_all[df_all['subject']==1]
+
+for idx,eccentricity in enumerate(eccentricity_array):
+    ecc_df = df[df['eccentricity']==eccentricity]
+
+    x_offset = (idx-1)
+
+    if eccentricity in [8]:
+        continue
+    
+    for bleaching in bleaching_array:
+        
+        if bleaching==8:
+            label = 'ecc %d'%eccentricity
         else:
             label = None
-        
-        ecc_df = subject_df[subject_df['eccentricity']==eccentricity]
-        
-        vmin_arr = np.array(ecc_df['velocity min'])
-        vmax_arr = np.array(ecc_df['velocity max'])
+            
+        bleach_df = ecc_df[ecc_df['bleaching']==bleaching]
+        vmin_arr = np.array(bleach_df['velocity min'])
+        vmax_arr = np.array(bleach_df['velocity max'])
+        ecc_marker = eccentricity_marker_array[idx]
 
-        plt.plot(eccentricity+x_offset,np.mean(vmax_arr),color_marker,label=label)
-        plt.errorbar(eccentricity+x_offset,np.mean(vmax_arr),yerr=np.std(vmax_arr),ecolor='k',capsize=4)
-
-
+        plt.figure(3)
+        plt.plot(bleaching+x_offset,np.mean(vmin_arr),'k'+ecc_marker,label=label)
+        plt.errorbar(bleaching+x_offset,np.mean(vmin_arr),yerr=err(vmin_arr),ecolor='k',capsize=4)
+        plt.xlabel('bleaching %')
+        plt.ylabel('$v_{min}$')
         
+        plt.figure(4)
+        plt.plot(bleaching+x_offset,np.mean(vmax_arr),'k'+ecc_marker,label=label)
+        plt.errorbar(bleaching+x_offset,np.mean(vmax_arr),yerr=err(vmax_arr),ecolor='k',capsize=4)
+        plt.xlabel('bleaching %')
+        plt.ylabel('$v_{max}$')
+        
+plt.figure(3)
+plt.legend()
+plt.figure(4)
 plt.legend()
 plt.show()
