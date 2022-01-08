6102361f0ce00af1ae4e59066d27667422e6ef04
Ravi Sankar Jonnal <rjonnal@gmail.com>
2022-01-07 16:22:16 -0800

    added configurability of plots to org_summary_plot.py

--- scripts/org_summary_plot.py
+++ scripts/org_summary_plot.py
@@ -2,10 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 import sys
+import octoblob.plotting_functions as opf
 
 
-edf = pd.read_csv('experimental_parameters_log.csv')
+
+edf = pd.read_csv('experimental_parameters_log_testing.csv')
 adf = pd.read_csv('analysis_parameters_log.csv')
+
+bleaching_subject = 1
 
 # the analysis parameters log contains the following figures of merit; for any run of this program, we must specify
 # which figures of merit to plot; a separate figure will be created for each figure of merit
@@ -16,9 +20,57 @@ adf = pd.read_csv('analysis_parameters_log.csv')
 # 'amax_0_50': the maximum (most positive) change in velocity (acceleration) between 0 and 50 ms
 # 'std_0_50': the standard deviation of the response between 0 and 50 ms
 # 'mad_0_50': the mean absolute deviation of the response between 0 and 50 ms
-figures_of_merit = ['vmin_0_20','vmean_20_40','amax_0_50']
+figures_of_merit = ['vmin_0_20','vmean_20_40','amax_0_50', 'amin_0_50']
 eccentricities_to_omit = []
 x_offset_fraction = 0.02
+figure_size = (4,3) # w,h in inches
+label_dict = {}
+label_dict['vmin_0_20'] = '$v_{min}$ ($\mu m/s$)'
+label_dict['vmax_20_40'] = '$v_{max}$ ($\mu m/s$)'
+label_dict['vmean_20_40'] = '$\overline{v_{20,40}}$ ($\mu m/s$)'
+label_dict['amin_0_50'] = '$(\Delta v)_{min}$ ($\mu m/s^2$)'
+label_dict['amax_0_50'] = '$(\Delta v)_{max}$ ($\mu m/s^2$)'
+label_dict['std_0_50'] = '$\sigma_{0,50}$ ($\mu m/s$)'
+label_dict['mad_0_50'] = '$\overline{|v|_{0,50}}$ ($\mu m/s$)'
+
+ylim_dict = {}
+ylim_dict['vmin_0_20'] = [-3.5,0]
+ylim_dict['vmax_20_40'] = [0,3]
+ylim_dict['vmean_20_40'] = [0,2]
+ylim_dict['amin_0_50'] = [-2.5,0]
+ylim_dict['amax_0_50'] = [0,1.75]
+ylim_dict['std_0_50'] = [0,3]
+ylim_dict['mad_0_50'] = [0,3]
+
+figure_size = (4,3) # (width_inches, height_inches)
+font = 'Arial'
+font_size = 10
+screen_dpi = 100
+print_dpi = 300
+plot_linewidth = 2
+
+# spines (lines at edges of plot)
+spine_color = 'k'
+spine_linewidth = 2
+
+# legend appearance
+show_legend_frame = True
+legend_linewidth = 2
+legend_edgecolor = 'k'
+legend_facecolor = 'w'
+legend_alpha = 1.0
+
+# see https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
+#style = 'bmh'
+style = 'ggplot'
+#style = 'seaborn-deep'
+#style = 'fivethirtyeight'
+#############################################################################################################
+
+opf.setup_plots(style=style,font_size=font_size,font=font)
+
+for fig_idx in range(1,2*len(figures_of_merit)+1):
+    plt.figure(figsize=figure_size)
 
 
 df_list = []
@@ -49,17 +101,9 @@ for aidx,arow in adf.iterrows():
     df_list.append(d)
 
 df_all = pd.DataFrame(df_list)
+df_all.to_csv('merged_parameters_log.csv')
 
 
-
-label_dict = {}
-label_dict['vmin_0_20'] = '$v_{min}$'
-label_dict['vmax_20_40'] = '$v_{max}$'
-label_dict['vmean_20_40'] = '$\overline{v_{20,40}}$'
-label_dict['amin_0_50'] = '$(\Delta v)_{min}$'
-label_dict['amax_0_50'] = '$(\Delta v)_{max}$'
-label_dict['std_0_50'] = '$\sigma_{0,50}$'
-label_dict['mad_0_50'] = '$\overline{|v|_{0,50}}$'
 
 subject_array = list(df_all['subject'].unique())
 
@@ -74,6 +118,13 @@ ecc_offset_factor = ecc_range*x_offset_fraction
 
 b_range = np.max(bleaching_array)-np.min(bleaching_array)
 b_offset_factor = b_range*x_offset_fraction
+
+def format_legend(leg):
+    leg.get_frame().set_edgecolor(legend_edgecolor)
+    leg.get_frame().set_facecolor(legend_facecolor)
+    leg.get_frame().set_linewidth(legend_linewidth)
+    leg.get_frame().set_alpha(legend_alpha)
+
 
 def err(vec):
     return np.std(vec)/np.sqrt(len(vec))
@@ -110,38 +161,66 @@ for idx,subject in enumerate(subject_array):
             
 # done with df, so delete and reuse
 
-df = df_all[df_all['subject']==1]
+df = df_all[df_all['subject']==bleaching_subject]
+mult = 1
+if len(df)>0:
+    mult = 2
+    for idx,eccentricity in enumerate(eccentricity_array):
+        ecc_df = df[df['eccentricity']==eccentricity]
 
-for idx,eccentricity in enumerate(eccentricity_array):
-    ecc_df = df[df['eccentricity']==eccentricity]
+        x_offset = (idx-1)*b_offset_factor
 
-    x_offset = (idx-1)*b_offset_factor
+        if eccentricity in eccentricities_to_omit:
+            continue
 
-    if eccentricity in eccentricities_to_omit:
-        continue
-    
-    for bidx,bleaching in enumerate(bleaching_array):
+        for bidx,bleaching in enumerate(bleaching_array):
+
+            if bidx==0:
+                label = 'ecc %d'%eccentricity
+            else:
+                label = None
+
+
+            bleach_df = ecc_df[ecc_df['bleaching']==bleaching]
+            ecc_marker = eccentricity_marker_array[idx]
+
+            for fidx,fom in enumerate(figures_of_merit):
+                y_arr = np.array(bleach_df[fom])
+                plt.figure(len(figures_of_merit)+fidx+1)
+                plt.plot(bleaching+x_offset,np.mean(y_arr),'k'+ecc_marker,label=label)
+                plt.errorbar(bleaching+x_offset,np.mean(y_arr),yerr=err(y_arr),ecolor='k',capsize=4)
+                plt.xlabel('bleaching %')
+                plt.ylabel(label_dict[fom])
+
+
+for f in range(1,mult*len(figures_of_merit)+1):
+    fom = figures_of_merit[(f-1)%len(figures_of_merit)]
+    try:
+        plt.figure(f)
+        leg = plt.legend()
+        format_legend(leg)
         
-        if bidx==0:
-            label = 'ecc %d'%eccentricity
+        ax = plt.gca()
+        ax.tick_params(direction='in')
+        ax.tick_params(left=True)
+        ax.tick_params(right=True)
+        ax.tick_params(top=True)
+        ax.tick_params(bottom=True)
+        ax.set_ylim(ylim_dict[fom])
+
+        for spine in ['top','bottom','left','right']:
+            ax.spines[spine].set_color(spine_color)
+            ax.spines[spine].set_linewidth(spine_linewidth)
+
+        plt.tight_layout()
+
+        
+        if f<=len(figures_of_merit):
+            plt.savefig('ecc_dependence_%s.svg'%fom)
         else:
-            label = None
-
-        
-        bleach_df = ecc_df[ecc_df['bleaching']==bleaching]
-        ecc_marker = eccentricity_marker_array[idx]
-
-        for fidx,fom in enumerate(figures_of_merit):
-            y_arr = np.array(bleach_df[fom])
-
-            plt.figure(len(figures_of_merit)+fidx+1)
-            plt.plot(bleaching+x_offset,np.mean(y_arr),'k'+ecc_marker,label=label)
-            plt.errorbar(bleaching+x_offset,np.mean(y_arr),yerr=err(y_arr),ecolor='k',capsize=4)
-            plt.xlabel('bleaching %')
-            plt.ylabel(label_dict[fom])
-
-for f in range(1,2*len(figures_of_merit)+1):
-    plt.figure(f)
-    plt.legend()
+            plt.savefig('bleaching_dependence_%s.svg'%fom)
+    except:
+        pass
+    
         
 plt.show()
