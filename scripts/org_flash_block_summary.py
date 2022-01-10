292148aff59855e2f4c4a26dd897e39c5cb674f6
Ravi Sankar Jonnal <rjonnal@gmail.com>
2022-01-10 13:16:15 -0800

    minor fix in summary filename specification

--- scripts/org_flash_block_summary.py
+++ scripts/org_flash_block_summary.py
@@ -2,7 +2,9 @@
 # to store output from all the analysis/visualization scripts
 
 # E.g.:
-# summary_datafile = 'C:\analysis_parameters_log.csv'
+# summary_datafile = 'C:/analysis_parameters_log.csv'
+# if using DOS style separator (\), you have to put a second \ to escape:
+# summary_datafile = 'C:\\analysis_parameters_log.csv'
 # summary_datafile = '/home/rjonnal/Dropbox/Data/conventional_org/flash/analysis_parameters_log.csv'
 
 summary_datafile = ''
