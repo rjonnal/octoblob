adb4a0f3a471facdb85d69598477257ec26d6e7b
Ravi Sankar Jonnal <rjonnal@gmail.com>
2022-01-19 22:23:03 -0800

    progress on summary plot

--- scripts/org_flash_block_make_summary_figure.py
+++ scripts/org_flash_block_make_summary_figure.py
@@ -37,13 +37,24 @@ label_dict['mad_0_50'] = '$\overline{|v|_{0,50}}$ ($\mu m/s$)'
 
 autoscale = True
 ylim_dict = {}
-ylim_dict['vmin_0_20'] = [-3.5,0]
-ylim_dict['vmax_20_40'] = [0,3]
-ylim_dict['vmean_20_40'] = [0,2]
-ylim_dict['amin_0_50'] = [-2.5,0]
-ylim_dict['amax_0_50'] = [0,1.75]
-ylim_dict['std_0_50'] = [0,3]
-ylim_dict['mad_0_50'] = [0,3]
+zero_overshoot = 1.5
+ylim_dict['vmin_0_20'] = [-4,zero_overshoot]
+ylim_dict['vmax_20_40'] = [-zero_overshoot,3]
+ylim_dict['vmean_20_40'] = [-zero_overshoot,2]
+ylim_dict['amin_0_50'] = [-2.5,zero_overshoot]
+ylim_dict['amax_0_50'] = [-zero_overshoot,2.5]
+ylim_dict['std_0_50'] = [-zero_overshoot,3]
+ylim_dict['mad_0_50'] = [-zero_overshoot,3]
+
+legendloc_dict = {}
+legendloc_dict['vmin_0_20'] = 4
+legendloc_dict['vmax_20_40'] = 1
+legendloc_dict['vmean_20_40'] = 1
+legendloc_dict['amin_0_50'] = 1
+legendloc_dict['amax_0_50'] = 1
+legendloc_dict['std_0_50'] = 1
+legendloc_dict['mad_0_50'] = 1
+
 
 # edit filenames below as needed:
 edf = pd.read_csv('experimental_parameters_log.csv')
@@ -51,8 +62,9 @@ adf = pd.read_csv('analysis_parameters_log.csv')
 
 figure_size = (4,4) # (width_inches, height_inches)
 ax_box = [.18,.15,.77,.80]
-ax_box_log_zero = [.18,.15,.2,.80]
-ax_box_log_nonzero = [.4,.15,.55,.80]
+nax_box = [.18,.15,.64,.80]
+ax_box_log_zero = [.18,.15,.15,.80]
+ax_box_log_nonzero = [.35,.15,.47,.80]
 
 font = 'Arial'
 font_size = 10
@@ -62,7 +74,7 @@ plot_linewidth = 2
 
 # spines (lines at edges of plot)
 spine_color = 'k'
-spine_linewidth = 2
+spine_linewidth = 1
 
 # legend appearance
 show_legend_frame = True
@@ -89,10 +101,13 @@ normalized_color = 'g'
 plot_normalized = True
 normalize_text = lambda x: x.replace('\mu m','\mathrm{OS}')
 
+
 subject_marker_array = ['o','s','d']
-ecc_marker_array = ['^','v','<','>']
+ecc_marker_array = ['o','s','d','*']
 
 eccentricities_to_omit = []
+
+bleaching_logx = True
 
 rebuild = False
 ##############################################################################
@@ -100,7 +115,7 @@ rebuild = False
 
 # Basic plot setup:
 opf.setup_plots(style=style,font_size=font_size,font=font)
-
+plt.rc('axes', axisbelow=True)
 
 # Function for calculating error bars:
 def err(vec,mode='SEM'):
@@ -109,7 +124,15 @@ def err(vec,mode='SEM'):
     elif mode=='SD':
         return np.std(vec)
 
+# Function for formatting legends:
+def format_legend(leg):
+    leg.get_frame().set_edgecolor(legend_edgecolor)
+    leg.get_frame().set_facecolor(legend_facecolor)
+    leg.get_frame().set_linewidth(legend_linewidth)
+    leg.get_frame().set_alpha(legend_alpha)
 
+
+    
 #################################
 # calculate oct sampling interval
 #################################
@@ -178,61 +201,6 @@ def unique(LL):
                 output.append(item)
     return output
 
-class DataSeries:
-
-    def __init__(self,xlabel,ylabel,xarr):
-
-        self.xlabel = xlabel
-        self.ylabel = ylabel
-        self.xarr = np.array(xarr)
-        self.yarr = [[]]*len(xarr)
-        self.larr = [[]]*len(xarr)
-        self.npoints = 0
-        
-    def add_point(self,x,y,label='foo'):
-        idx = np.where(self.xarr==x)[0][0]
-        print(y)
-        self.yarr[idx].append(y)
-        self.larr[idx].append(label)
-        self.npoints += 1
-        print(idx,y,self.npoints)
-        
-    def __str__(self):
-        return 'DataSeries %s %s'%(self.xlabel,self.ylabel)
-
-    def linplot(self,ax,markers=['s','o','x','d','.','^','v','<','>'],color='k'):
-        unique_labels = unique(self.larr)
-
-        xr = np.max(self.xarr)-np.min(self.xarr)
-        xoffset = xr*x_offset_fraction
-        
-        for label_index,(label,marker) in enumerate(zip(unique_labels,markers)):
-            color_marker = color+marker
-            xo = label_index-len(unique_labels)//2
-            ymean = []
-            yerr = []
-            for ylist,llist in zip(self.yarr,self.larr):
-                ytemp = []
-                for yitem,litem in zip(ylist,llist):
-                    if litem==label:
-                        ytemp.append(yitem)
-                ymean.append(np.mean(ytemp))
-                yerr.append(err(ytemp))
-
-                plt.imshow(np.array(self.yarr),aspect='auto')
-                plt.show()
-                sys.exit()
-                
-                print(len(ylist))
-                print(len(ytemp))
-                
-                sys.exit()
-            sys.exit()
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
-            
-
-
 subject_array = list(df_all['subject'].unique())
 ecc_array = sorted(list(df_all['eccentricity'].unique()))
 bleaching_array = sorted(list(df_all['bleaching'].unique()))
@@ -250,30 +218,147 @@ except Exception:
 ecc_ds_dict = {}
 b_ds_dict = {}
 
-# Build the ecc data series:
-xr = np.max(ecc_array)-np.min(ecc_array)
+exr = np.max(ecc_array)-np.min(ecc_array)
+bxr = np.max(bleaching_array)-np.min(bleaching_array)
+
+# Build the ecc data plots:
 for fom in figures_of_merit:
     fig = plt.figure(figsize=figure_size)
-    ax = fig.add_axes(ax_box)
+    
+    if plot_normalized:
+        ax = fig.add_axes(nax_box)
+        nax = ax.twinx()
+        nax.yaxis.set_label_position('right')
+        nax.yaxis.tick_right()
 
+        nax.yaxis.label.set_color(normalized_color)
+        nax.tick_params(axis='y', colors=normalized_color)
+        #nax.spines['right'].set_color(normalized_color)
+
+    else:
+        ax = fig.add_axes(ax_box)
+        
     # each subject is one series in this plot
     for sidx,subject in enumerate(subject_array):
-        xo = xr*x_offset_fraction*(sidx-len(subject_array)//2)
-        color_marker = 'k'+subject_marker_array[sidx]
+        xo = exr*x_offset_fraction*(sidx-len(subject_array)//2)
+        marker = subject_marker_array[sidx]
+        color_marker = 'k'+marker
+        ncolor_marker = normalized_color+subject_marker_array[sidx]
         ymean = []
         yerr = []
+        nymean = []
+        nyerr = []
         for ecc in ecc_array:
             rows = df_all[(df_all['bleaching']==66)&(df_all['subject']==subject)&(df_all['eccentricity']==ecc)]
             ydat = rows[fom]
             ymean.append(np.mean(ydat))
             yerr.append(err(ydat))
+            norm = rows[normalization_column]
+            nydat = ydat/norm
+            nymean.append(np.mean(nydat))
+            nyerr.append(err(nydat))
             
-        ax.plot(ecc_array+xo,ymean,color_marker,label='subject %d'%subject)
+            
+        ax.plot(ecc_array+xo,ymean,marker=marker,markerfacecolor='k',
+                label='subject %d'%subject,linestyle='none',markeredgecolor='none')
         ax.errorbar(ecc_array+xo,ymean,yerr=yerr,ecolor='k',capsize=4,linestyle='none')
-            
-    ax.legend()
-    plt.show()
 
+        if plot_normalized:
+            nax.plot(ecc_array+xo,nymean,marker=marker,markerfacecolor=normalized_color,
+                     linestyle='none',markeredgecolor='none')
+            #nax.plot(ecc_array+xo,nymean,ncolor_marker)
+            nax.errorbar(ecc_array+xo,nymean,yerr=nyerr,ecolor=normalized_color,capsize=4,linestyle='none')
+
+    ax.set_ylabel(label_dict[fom])
+    ax.set_ylim(ylim_dict[fom])
+    if plot_normalized:
+        nax.set_ylabel(normalize_text(label_dict[fom]))
+        nax.set_yticks(ax.get_yticks())
+        #nax.set_yticks([])
+        nax.set_ylim(ylim_dict[fom])
+        
+    for spine in ['top','bottom','left','right']:
+        nax.spines[spine].set_visible(False)
+        ax.spines[spine].set_color(spine_color)
+        ax.spines[spine].set_linewidth(spine_linewidth)
+
+    ax.set_axisbelow(True)
+    if plot_normalized:
+        nax.set_axisbelow(True)
+    leg = ax.legend(loc=legendloc_dict[fom])
+    format_legend(leg)
+    nax.yaxis.grid(False)
+    nax.xaxis.grid(False)
+
+
+# Build the bleaching data plots:
+for fom in figures_of_merit:
+    fig = plt.figure(figsize=figure_size)
+
+    if bleaching_logx:
+        lax = fig.add_axes(ax_box_log_zero)
+        lax.set_xlim([-.5,.5])
+        lax.set_xticks([0])
+        rax = fig.add_axes(ax_box_log_nonzero)
+        rax.yaxis.set_label_position('right')
+        rax.yaxis.tick_right()
+    else:
+        lax = fig.add_axes(ax_box)
+    
+    # each ecc is one series in this plot
+    for eidx,ecc in enumerate(ecc_array):
+        centered_idx = eidx-len(ecc_array)//2+0.5
+        xo = bxr*x_offset_fraction*centered_idx*0.1
+        marker = ecc_marker_array[eidx]
+        ymean = []
+        yerr = []
+        for bleaching in bleaching_array:
+            rows = df_all[(df_all['bleaching']==bleaching)&(df_all['subject']==bleaching_subject)&(df_all['eccentricity']==ecc)]
+            ydat = rows[fom]
+            ymean.append(np.mean(ydat))
+            yerr.append(err(ydat))
+
+        if bleaching_logx:
+            for b,ym,ye in zip(bleaching_array,ymean,yerr):
+                xolog = centered_idx*np.log10(bleaching)*3.5*x_offset_fraction
+                if b==0:
+                    lax.plot(b+xo,ym,marker=marker,markerfacecolor='w',
+                             label='$%d^\circ$'%ecc,linestyle='none',markeredgecolor='k')
+                else:
+                    rax.semilogx(b+xolog,ym,marker=marker,markerfacecolor='w',
+                             linestyle='none',markeredgecolor='k')
+        else:
+            lax.plot(bleaching_array+xo,ymean,marker=marker,markerfacecolor='k',
+                     linestyle='none',markeredgecolor='none',label='$%d^\circ$'%ecc)
+
+    lax.set_ylabel(label_dict[fom])
+    lax.set_ylim(ylim_dict[fom])
+    if bleaching_logx:
+        rax.set_ylim(ylim_dict[fom])
+        
+    for spine in ['top','bottom','left','right']:
+        lax.spines[spine].set_color(spine_color)
+        lax.spines[spine].set_linewidth(spine_linewidth)
+        if bleaching_logx:
+            rax.spines[spine].set_color(spine_color)
+            rax.spines[spine].set_linewidth(spine_linewidth)
+            if spine=='left':
+                rax.spines[spine].set_visible(False)
+            if spine=='right':
+                lax.spines[spine].set_visible(False)
+
+    if bleaching_logx:
+        leg = rax.legend(loc=legendloc_dict[fom])
+    else:
+        leg = lax.legend(loc=legendloc_dict[fom])
+        
+    format_legend(leg)
+
+
+    
+plt.show()
+
+    
 sys.exit()
 
 for idx,subject in enumerate(subject_array):
@@ -358,12 +443,6 @@ for fig_idx in range(1,len(figures_of_merit)+1):
     
 
 
-
-def format_legend(leg):
-    leg.get_frame().set_edgecolor(legend_edgecolor)
-    leg.get_frame().set_facecolor(legend_facecolor)
-    leg.get_frame().set_linewidth(legend_linewidth)
-    leg.get_frame().set_alpha(legend_alpha)
 
 # first, let's look at ecc-dependence, with only 66% bleaching
 df = df_all[df_all['bleaching']==66]
